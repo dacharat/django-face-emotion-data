a61d44c76d0589625125429643c48fc361b955f3
dacharat <dacharat.p@ku.th>
2019-07-09 13:12:26 +0200

    :tada: fixed admin page not found

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('graph.urls')),
     path('admin/', admin.site.urls),
+    path('', include('graph.urls')),
 ]
